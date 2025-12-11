04724fa64b54c24a367e61b8139f62970eb907ef
KamillKAPLAN <kamilkaplnn@gmail.com>
2025-12-11 17:29:11 +0300

    spring graphQL project created

---
+++ spring-graphQL/src/test/java/com/kkaplan/spring_graphQL/SpringGraphQlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkaplan.spring_graphQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGraphQlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
