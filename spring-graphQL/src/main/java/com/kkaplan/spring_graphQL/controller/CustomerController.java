cca2de4d51ecc250a9d2bf7b3767785d9cf9e4f8
KamillKAPLAN <kamilkaplnn@gmail.com>
2025-12-16 20:58:21 +0300

    GraphQL controller eklendir

---
+++ spring-graphQL/src/main/java/com/kkaplan/spring_graphQL/controller/CustomerController.java
@@ -0,0 +1,37 @@
+package com.kkaplan.spring_graphQL.controller;
+
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import com.kkaplan.spring_graphQL.data.Customer;
+import com.kkaplan.spring_graphQL.data.CustomerRepository;
+
+@Controller
+public class CustomerController {
+    private final CustomerRepository customerRepository;
+
+    public CustomerController(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+    
+    /* http://localhost:8080/graphiql
+     * GraphQL Playground Chrome Extension
+     */
+
+    @QueryMapping
+    public Iterable<Customer> customers() {
+        return customerRepository.findAll();
+    }
+
+    @QueryMapping
+    public Customer customerById(@Argument Long id) {
+        return customerRepository.findById(id).orElseThrow();
+    }
+
+    @QueryMapping
+    public Customer customerByEmail(@Argument String email) {
+    	
+        return customerRepository.findCustomerByEmail(email);
+    }
+}
