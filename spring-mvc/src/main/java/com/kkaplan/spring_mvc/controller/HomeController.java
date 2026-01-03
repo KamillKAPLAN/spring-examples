0558a04af4c0332d663914c084e9611d0b3307e8
KamillKAPLAN <kamilkaplnn@gmail.com>
2026-01-03 14:08:00 +0300

    link html pages from the home

--- spring-mvc/src/main/java/com/kkaplan/spring_mvc/controller/HomeController.java
+++ spring-mvc/src/main/java/com/kkaplan/spring_mvc/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.kkaplan.spring_mvc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class HomeController {
@@ -11,4 +12,9 @@ public class HomeController {
 	public String displayName(Model model) {
 		return "index";
 	}
+	
+	@GetMapping("/getCharacter/{charname}")
+    public String getCharacter(@PathVariable("charname") String charName){
+        return "/characters/"+charName;
+    }
 }
